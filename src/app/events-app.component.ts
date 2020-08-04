bd21e0c6ad15fd0fdea35d39729a95ab9d7eec44
Pali Thabo <tpali@cut.ac.za>
2020-08-04 16:11:14 +0200

    Initial Commit Again

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h2>Hello World</h2>',
+  template: '<h1>Hello World</h1>',
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
